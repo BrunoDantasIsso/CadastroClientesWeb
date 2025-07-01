7c5a3123786407dc8cf8344acb4fedd09174cee7
BrunoDantasIsso <brunodantas.isso@gmail.com>
2025-06-30 21:53:05 -0300

    Correções nas views create e delete e correção no model Cliente

--- CadastroClientesWeb/Models/Cliente.cs
+++ CadastroClientesWeb/Models/Cliente.cs
@@ -5,7 +5,9 @@ namespace CadastroClientesWeb.Models
     public class Cliente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Nome obrigatório para cadastro")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "E-mail obrigatório para cadastro")]
         public string Email { get; set; }
         public ICollection<Telefone>? Telefones { get; set; } = new List<Telefone>();
         public Endereco? Endereco { get; set; }
